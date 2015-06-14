c4d5ab84a9238d7375c0eee1077258f8a2fe5923
Christopher Tam <ohgodtamit@gmail.com>
2015-06-14 14:25:31 -0400

    Reworking project...

---
+++ GoogleMusicAPI.NET/Clients/WebClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMusicAPI.Clients
+{
+    class WebClient
+    {
+
+        #region Constructor
+
+        public WebClient(bool validate = true, bool verifySSL = true)
+        {
+
+        }
+
+        #endregion
+
+
+        #region Login
+
+        public bool Login(string email, string password)
+        {
+
+        }
+
+        #endregion
+    }
+}
