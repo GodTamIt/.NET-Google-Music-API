5a004b57a347e3cc2020a870af76c5573dca394b
Christopher Tam <ohgodtamit@gmail.com>
2015-06-15 15:27:01 -0400

    Create ExceptionWriter class to handle human-readable errors
    ExceptionWriter class provides extensions to Exceptions' ToString methods.  ExceptionWriter is a static class that is internal to the API and cannot be instantiated.

---
+++ GoogleMusicAPI.NET/ExceptionWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMusic
+{
+    /// <summary>
+    /// Formats exceptions into human-readable formats.
+    /// </summary>
+    internal static class ExceptionWriter
+    {
+        public static string ToString(this System.Net.WebException ex, string message)
+        {
+            string details = (ex.Response == null ? ex.Message : "The server response was: " + ex.Response);
+            return String.Format("{0}\n\n{1}", message, details);
+        }
+
+        public static string ToString(this Exception ex, string message)
+        {
+            return String.Format("{0}\n\n{1}", message, ex.Message);
+        }
+    }
+}
