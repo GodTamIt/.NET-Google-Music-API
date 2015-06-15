fc45a38b9821dabdcf9da114ebe4fb85d7a85a92
Christopher Tam <ohgodtamit@gmail.com>
2015-06-15 15:22:52 -0400

    Create IClient interface for GoogleMusic.Clients

---
+++ GoogleMusicAPI.NET/Clients/IClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMusic.Clients
+{
+    public interface IClient
+    {
+        void Logout();
+
+        /// <summary>
+        /// Gets whether the client is logged in.
+        /// </summary>
+        bool IsLoggedIn { get; }
+    }
+}
