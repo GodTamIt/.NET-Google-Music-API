7c3d3ef0c69c53403d06ed1d85bdaf9fa3c9eeaf
Christopher Tam <ohgodtamit@gmail.com>
2015-06-21 17:17:27 -0400

    Add and fix Playlist properties, revert Playlist songs container to List

--- GoogleMusicAPI.NET/Models/Playlist.cs
+++ GoogleMusicAPI.NET/Models/Playlist.cs
@@ -11,59 +11,84 @@ namespace GoogleMusic
     
     public class Playlist
     {
+
         #region Properties: Info
 
         [JsonProperty("title")]
         public string Title { get; set; }
 
         [JsonProperty("id")]
-        public Guid ID { get; protected set; }
+        public Guid ID { get; protected internal set; }
 
         [JsonProperty("creationTimestamp")]
-        public long CreationTimestampMicroseconds { get; protected set; }
+        public long CreationTimestampMicroseconds { get; protected internal set; }
 
         [JsonProperty("recentTimestamp")]
-        public long RecentTimestampMicrseconds { get; protected set; }
+        public long RecentTimestampMicrseconds { get; protected internal set; }
 
         [JsonProperty("type")]
-        public int Type { get; protected set; }
+        public int Type { get; protected internal set; }
 
         [JsonProperty("sharedToken")]
-        public string SharedToken { get; protected set; }
+        public string SharedToken { get; protected internal set; }
 
         [JsonProperty("lastModifiedTimestamp")]
-        public long LastModifiedTimestampMicroseconds { get; protected set; }
+        public long LastModifiedTimestampMicroseconds { get; protected internal set; }
 
         [JsonProperty("description")]
         public string Description { get; set; }
 
         [JsonProperty("ownerName")]
-        public string OwnerName { get; protected set; }
+        public string OwnerName { get; protected internal set; }
 
         [JsonProperty("playlistArtUrl")]
         public string[] PlaylistArtUrl { get; set; }
 
         [JsonProperty("ownerProfilePhotoUrl")]
-        public string OwnerProfilePhotoUrl { get; protected set; }
+        public string OwnerProfilePhotoUrl { get; protected internal set; }
 
         [JsonProperty("suggestedPlaylistArtUrl")]
-        public string[] SuggestedPlaylistArtUrl { get; protected set; }
+        public string[] SuggestedPlaylistArtUrl { get; protected internal set; }
 
         [JsonProperty("requestTime")]
         public double RequestTime { get; set; }
 
         #endregion
 
+        #region Properties: Calculated
+
+        public DateTime CreationTimestamp
+        {
+            get { return FromUnixMicroseconds(CreationTimestampMicroseconds); }
+        }
+
+        public DateTime RecentTimestamp
+        {
+            get { return FromUnixMicroseconds(RecentTimestampMicrseconds); }
+        }
+
+        public DateTime LastModifiedTimestamp
+        {
+            get { return FromUnixMicroseconds(LastModifiedTimestampMicroseconds); }
+        }
+
+        private static DateTime FromUnixMicroseconds(long microseconds)
+        {
+            return new DateTime(1970, 01, 01).AddMilliseconds(microseconds / 1000).ToLocalTime();
+        }
+
+        #endregion
+
         #region Properties: Songs
 
-        private Dictionary<Guid, Song> _songs;
-        public Dictionary<Guid, Song> Songs
+        private List<Song> _songs;
+        public List<Song> Songs
         {
             get { return _songs; }
             set
             {
                 if (value == null && _songs != null)
-                    _songs = new Dictionary<Guid, Song>();
+                    _songs = new List<Song>();
                 else
                     _songs = value;
             }
