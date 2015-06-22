488a58d1770c75d2a62c23691d64224da3b26e01
Christopher Tam <ohgodtamit@gmail.com>
2015-06-22 01:25:50 -0400

    Fix minor build details

--- GoogleMusicAPI.NET/Properties/AssemblyInfo.cs
+++ GoogleMusicAPI.NET/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GoogleMusicAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2013-2015")]
+[assembly: AssemblyCopyright("Copyright © Christopher Tam 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.4.*")]
-[assembly: AssemblyFileVersion("4.4.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
